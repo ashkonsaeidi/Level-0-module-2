0da1a06280b8e80f8a523f40ea8fef1cc29c5d1b
jereycui2008 <jeremycuid@gmail.com>
2023-03-03 18:24:12 -0800

    I made a validation and magic 8 ball code today

--- _02_random/_4_validation/validation.py
+++ _02_random/_4_validation/validation.py
@@ -4,16 +4,21 @@ from tkinter import messagebox, Tk
 if __name__ == '__main__':
     window = Tk()
     window.withdraw()
-
-    random_number = random.randint(1, 5)
-
-    print(random_number)
+    for i in range(10):
+        random_number = random.randint(1, 5)
+        print(random_number)
 
     # TODO 1) Use each value of random_number to give the user a random compliment
-    if random_number == '2':
-        messagebox.showinfo(None, message="you look like a zebra with wings")
-    if random_number = '3':
-        messagebox.showinfo(None,message="You look like a beauty in the forest")
+        if random_number == 1:
+            messagebox.showinfo(None, message="I like your hair")
+        if random_number == 2:
+            messagebox.showinfo(None, message="you look like a zebra with wings")
+        if random_number == 3:
+            messagebox.showinfo(None,message="You look like a beauty in the forest")
+        if random_number == 4:
+            messagebox.showinfo(None, message="You looking fine zesty")
+        if random_number == 5:
+            messagebox.showinfo(None, message="I love you baby girl")
     # TODO 2) Repeat all the code above 10 times
 
     # TODO 3) Find someone to test out your program. They will like it :)
--- _02_random/_5_magic_8_ball/magic_8_ball.py
+++ _02_random/_5_magic_8_ball/magic_8_ball.py
@@ -6,21 +6,28 @@ if __name__ == '__main__':
     window.withdraw()
 
     # TODO Get the user to enter a question for the 8 ball to answer
-
+    question = simpledialog.askstring(None, prompt="Enter a question for the magic 8 ball to answer")
     # Make a variable and initialize it to a random number between 0 and 3
-
+    number = random.randint(0,3)
+    print(number)
     # If the random number is 0
+    if number == 0:
+        messagebox.showinfo(None, message="Yes")
 
         # tell the user "Yes"
 
     # If the random number is 1
-
+    if number == 1:
+        messagebox.showinfo(None, message="No")
         # tell the user "No"
 
     # If the random number is 2
-
+    if number == 2:
+        messagebox.showinfo(None, message="Maybe ask google?")
         # tell the user "Maybe you should ask Google?"
 
     # If the random number is 3
 
         # write your own answer
+    if number == 3:
+        message
