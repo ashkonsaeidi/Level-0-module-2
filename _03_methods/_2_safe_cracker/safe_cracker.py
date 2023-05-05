cf643b3ab01ca3c7094e3147b92b9478cad76cfa
league <alishahussain1229@gmail.com>
2023-05-05 16:56:24 -0700

    I made dot race

--- _03_methods/_2_safe_cracker/safe_cracker.py
+++ _03_methods/_2_safe_cracker/safe_cracker.py
@@ -8,13 +8,13 @@ def crack_the_safe():
     pass
     # TODO: Your mission: Use the try_code method to crack the safe
     #  by trying all possible combinations
-    Chicken = rando                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   m.randint(0, 999)
+chicken = random.randint(0, 999)
 
 
 def try_code(guess):
     print("trying " + str(guess))
 
-    secret_code = 999999 - Chicken
+    secret_code = 999999 - chicken
 
     if guess == secret_code:
         messagebox.showinfo(None, "Congratulations! You cracked the safe with " + str(guess))
@@ -24,35 +24,9 @@ def try_code(guess):
 
 def play_the_sound_of_success():
     playsound('me-gusta.wav')
-
-
-if __name__ == '__main__':
-    window = Tk()
-    window.withdraw()
-    crack_the_safe()
 
 
 # ======================= DO NOT EDIT THE CODE BELOW =========================
 
-wekncrzpasfdkjhcfjse = random.randint(0, 999)
 
 
-def try_code(guess):
-    print("trying " + str(guess))
-
-    secret_code = 999999 - wekncrzpasfdkjhcfjse
-
-    if guess == secret_code:
-        messagebox.showinfo(None, "Congratulations! You cracked the safe with " + str(guess))
-        play_the_sound_of_success()
-
-
-
-def play_the_sound_of_success():
-    playsound('me-gusta.wav')
-
-
-if __name__ == '__main__':
-    window = Tk()
-    window.withdraw()
-    crack_the_safe()
